9673ac9ed227e8e7cd1aa645c2046fd0a0621ce8
renc <renchao@oceanum.cn>
2023-11-09 18:19:30 +0800

    feat(kuber): kuber-gateway added

---
+++ kuber-gateway/src/main/java/org/example/kuber/gateway/Application.java
@@ -0,0 +1,15 @@
+package org.example.kuber.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author renc
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
