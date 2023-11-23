a26eaaed14b5cfdbf0c2bb14e67fa19832259c5f
lienider <lienider@icloud.com>
2023-11-24 00:28:35 +0800

    feat(kuber): logging file-lock test

--- kuber-controller/src/main/java/org/example/kuber/controller/Application.java
+++ kuber-controller/src/main/java/org/example/kuber/controller/Application.java
@@ -1,5 +1,7 @@
 package org.example.kuber.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -7,11 +9,20 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    private static final Logger PRUDENT_LOGGER = LoggerFactory.getLogger("PRUDENT_LOGGER");
+
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(Application.class, args);
+
+        for (;;) {
+            PRUDENT_LOGGER.info("1111111111");
+            TimeUnit.MILLISECONDS.sleep(10);
+        }
     }
 
     @RestController
